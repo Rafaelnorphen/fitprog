91ad0d32cd699940784629c6e33398b3a99d762d
Rafael Bartolome <132453419+Rafaelnorphen@users.noreply.github.com>
2024-06-11 16:06:01 -0300

    Add files via upload
    Aplicativo ainda em desenvolvimento. tela de login pronta

---
+++ slides.js
@@ -0,0 +1,25 @@
+export default [
+
+{
+    id: '1',
+    title: 'Treine melhor',
+    description: 'Treinar melhor traz resultados mais rápidos e eficazes, ajudando a aumentar a força e resistência',
+    image: require('./assets/workout.png')
+},
+
+
+{
+    id: '2',
+    title: 'Monitore seus resultados',
+    description: 'Acompanhe sua progressao de carga e periodização de treino. Não deixe resultados na mesa!',
+    image: require('./assets/tracker.png')
+},
+
+{
+    id: '3',
+    title: 'Aumente seus ganhos!',
+    description: 'progredir carga aumenta ganhos de massa muscular de cerca de 20% a 30% a mais em comparação com indivíduos que não utilizam esse método',
+    image: require('./assets/stats.png')
+}
+
+]
